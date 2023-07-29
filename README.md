ba5cf45ae00398c64179f708b4e981e0e0566893
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-07-29 00:26:02 -0300

    criando classes filme e serie (com duplicações de código)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # curso_python
+
+
+> pq usar @propertie?
+> Os properties são importantes, pois quem depende da classe não precisa mudar.
---
+++ entreterimento.py
@@ -0,0 +1,26 @@
+class Entreterimento:
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self,novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    def remover_like(self):
+        self.__likes -= 1
+
+    def mostrar_likes(self):
+        return self.likes
